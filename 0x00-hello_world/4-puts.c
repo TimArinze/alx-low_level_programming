e9c0df238f89c9ea2305071188ced5a331748b9d
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-02 15:08:42 +0000

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
+/*My main function*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return 0;
 }

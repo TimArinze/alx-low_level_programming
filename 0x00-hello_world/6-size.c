78711bc8163f4fb2780a3c4f43cb930c945baea4
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-02 16:26:43 +0000

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-	printf("Size of an int: %d\n", sizeof(int));
-	printf("Size of a char: %d\n", sizeof(char));
-	printf("Size of a float: %d\n", sizeof(float));
-	printf("Size of a double: %d\n", sizeof(double));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	printf("Size of a long: %d byte(s)\n", sizeof(long));
 	return (0);
 }

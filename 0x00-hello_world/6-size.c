afb9abff9b50c54c09188f0c7b1ddd62fbdd60df
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-02 16:16:31 +0000

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("Size of an int: %d\n", sizeof(int));
+	printf("Size of a char: %d\n", sizeof(char));
+	printf("Size of a float: %d\n", sizeof(float));
+	printf("Size of a double: %d\n", sizeof(double));
+	return (0);

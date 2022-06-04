00f8e606c0de65d9fcac9db1f2b2b085e31befe5
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-04 23:52:48 +0000

    for 101

---
+++ 0x01-variables_if_else_while/for_101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			for (c = 0; c < 10; c++)
+			{
+				if (a != b && b != c && a < b && b < c)
+				{
+					putchar('0' + a);
+					putchar('0' + b);
+					putchar('0' + c);
+					if (a + b + c != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

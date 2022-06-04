926d88f0909deede0e989c13de22c9f0ac37bd2c
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-04 00:10:48 +0000

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	char alphabet;
+
+	for (alphabet = 'a', alphabet <= 'z', ++alphabet)
+		putchar(alphabet);
+	return (0);
+}

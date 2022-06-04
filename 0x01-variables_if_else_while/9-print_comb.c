127c772c1c10ae415b5873293b600c0643d491b6
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-04 03:27:38 +0000

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,9 +9,11 @@ int main(void)
 	int n;
 
 	for (n = 0; n <= 9; n++)
-		putchar('0' + n)
-		putchar(',')
+	{
+		putchar('0' + n);
+		putchar(',');
 		putchar(' ');
+	}
 	putchar('\n');
 	return (0);
 }

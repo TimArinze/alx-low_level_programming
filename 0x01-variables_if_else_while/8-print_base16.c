9b5844cf6c3d0367fff459cc3f072988c1277100
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-04 02:45:33 +0000

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,12 +6,13 @@
  */
 int main(void)
 {
-	char c, n;
+	char c;
+	int n;
 
 	for (c = '0'; c <= 9; c++)
 		putchar(c);
 	for (n = 'a'; n <= 'f'; n++)
-		putchar(n);
+		putchar('0', n);
 	putchar('\n');
 	return (0);
 }

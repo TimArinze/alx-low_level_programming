ecd37d091cc3aa56cc507574a06206e0f38b9045
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-07 22:39:23 +0000

    Task 9

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - function that adds to integers and returns the result
+ *
+ * @n: function argument
+ *
+ * @m: function argument
+ *
+ * Return: Always return result
+ */
+int add(int n, int m)
+{
+	int result;
+
+	result = n + m;
+	return (result);
+}

95ac92cceaece4cecb4a57aad261f736a8952f86
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-07 16:00:32 +0000

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always void
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	int b;
+
+	for (b = 1; b <= 10; b++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n);
+	}
+}

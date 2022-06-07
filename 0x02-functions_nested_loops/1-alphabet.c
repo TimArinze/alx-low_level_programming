e376e3e0836c29750a381013bc47194f40bacc69
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-07 15:21:57 +0000

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.c"
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always void
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = "a"; a <= "z"; a++)
+	{
+		_putchar(a);
+	}
+	_putchar("\n");
+}

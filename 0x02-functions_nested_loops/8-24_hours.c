874d3c1b452098ce5b3b5271044f3a01fc477cce
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-07 19:46:58 +0000

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - function that prints every minute of the day
+ *
+ * Return: 0 always
+ */
+void jack_bauer(void)
+{
+	int a, b;
+
+	for (a = 0; a < 24; a++)
+	{
+		for (b = 0; b < 60; b++)
+		{
+			_putchar('0' + (a % 10));
+			_putchar('0' + (a / 10));
+			_putchar(':');
+			_putchar('0' + (b % 10));
+			_putchar('0' + (b / 10));
+		}
+		putchar('\n');
+	}
+}

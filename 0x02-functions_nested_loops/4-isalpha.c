485e2b58f26c1c66bf4d2940c08b17bb988dda2e
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-07 17:54:15 +0000

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,11 +4,11 @@
  *
  * @c: function argument.
  *
- * Returns: 1 if it is alphabetic and 0 if otherwise
+ * Return: 1 if it is alphabetic and 0 if otherwise
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a') && (c <= 'z') || (c >= 'A') && (c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);

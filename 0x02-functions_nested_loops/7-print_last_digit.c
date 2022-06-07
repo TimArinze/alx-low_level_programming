1c6cc2da5b6497d16871612a9de00e1a0776ef64
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-07 19:12:38 +0000

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - function that prints the last digit of a number
+ *
+ * @n: function
+ *
+ * Return: Always last digit
+ */
+int print_last_digit(int n)
+{
+	int number;
+
+	number = n % 10;
+	return (number);
+}

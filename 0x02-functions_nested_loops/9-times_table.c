b1d4d4dfb593ad9b8f671ef9ab3d4773ae90e8a5
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-07 20:36:17 +0000

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * times_table - function that prints times table
+ *
+ * Return: 0
+ */
+void times_table(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			_putchar('0' + (a * b));
+			_putchar(',');
+			_putchar(' ');
+		}
+		putchar('\n');
+	}
+}

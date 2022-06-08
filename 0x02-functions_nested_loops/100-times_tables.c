999715d8e0f66d4aa43e1ff194291e48cf378e1c
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-08 00:49:36 +0000

    Task 12

---
+++ 0x02-functions_nested_loops/100-times_tables.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_times_table - function that prints times time
+ *
+ * @n: function argument
+ *
+ * Return: Always 0
+ */
+void print_times_table(int n)
+{
+	int a, b;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				result = a * b;
+				if (b == n)
+				{
+					printf("%3d", result);
+				}
+				else
+				{
+					printf("%3d, ", result);
+				}
+			}
+			printf("\n");
+		}
+	}
+}

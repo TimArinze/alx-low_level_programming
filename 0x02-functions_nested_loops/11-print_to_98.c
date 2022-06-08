77f601ff5397215fc3a26f4682de4f890526f226
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-08 00:18:38 +0000

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,89 +8,22 @@
  */
 void print_to_98(int n)
 {
-	int a;
-
+	if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
 	if (n > 98)
 	{
-		for (a = n; a >= 98; a--)
+		for (; n > 98; n--)
 		{
-			if (a > 99)
-			{
-				_putchar('0' + (a / 100));
-				_putchar('0' + ((a / 10) % 10));
-				_putchar('0' + (a % 100));
-			}
-			if (a < 100)
-			{
-				_putchar('0' + (a / 10));
-				_putchar('0' + (a % 10));
-			}
-			if (a != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-	}
-	if (n < 98 && n >= 0)
-	{
-		for (a = n; a <= 98; a++)
-		{
-			if (a < 10)
-			{
-				_putchar('0' + a);
-			}
-			if (a > 10)
-			{
-				_putchar('0' + (a / 10));
-				_putchar('0' + (a % 10));
-			}
-			if (a != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-	}
-	if (n < 98 && n < 0)
-	{
-		for (a = n; a <= 98; a++)
-		{
-			if (a < 0)
-			{
-				a *= -1;
-				if (a > 9)
-				{
-					_putchar('0' + (a / 10));
-					_putchar('0' + (a % 10));
-				}
-				if (a <= 9)
-				{
-					_putchar('0' + a);
-				}
-			}
-			if (a >= 0)
-			{
-				if (a > 9)
-				{
-					_putchar('0' + (a / 10));
-					_putchar('0' + (a % 10));
-				}
-				if (a <= 9)
-				{
-					_putchar('0' + a);
-				}
-			}
-			if (a != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			printf("%d, ", n);
 		}
 	}
 	if (n == 98)
 	{
-		_putchar('0' + (a / 10));
-		_putchar('0' + (a % 10));
+		printf("%d\n", n);
 	}
 }

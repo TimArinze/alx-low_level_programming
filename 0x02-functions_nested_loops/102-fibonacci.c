4e3ef33e802bdf8eb6f0721cccc67ce4bdf9b84e
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-08 01:51:52 +0000

    Task 13

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int count = 2;
+	long int i = 1, j = 2, k;
+
+	printf("%d, ", i);
+	for (; count <= 50; count++)
+	{
+		if (count == 50)
+		{
+			printf("%d\n", j);
+		}
+		else
+		{
+			printf("%d, ", j);
+		}
+		k = j;
+		j += i;
+		i = k;
+	}
+	return (0);
+}

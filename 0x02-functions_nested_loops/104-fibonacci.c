b4d82174c02d12c9cb408ae325790060ec6f07a3
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-08 19:06:14 +0000

    Task 15

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,12 +11,12 @@ int main(void)
 	double first = 1, second = 2, next;
 
 	printf("%.0f, ", first);
-	for (c = 0; c <= 98; c++)
+	for (c = 0; c <= 97; c++)
 	{
 		next = second;
 		second += first;
 
-		if (c > 98)
+		if (c > 97)
 		{
 			printf("%.0f\n", second);
 			break;

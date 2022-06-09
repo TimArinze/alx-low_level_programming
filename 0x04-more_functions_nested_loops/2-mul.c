464ebeca6c6298f0a43ae21e41b9069602cf76c3
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-09 16:18:26 +0000

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplication function
+ * @a: variable
+ * @b: variable
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}

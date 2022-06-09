27bd403c1e1ca2fb8d77b994d7a00a49439a7860
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-09 19:48:34 +0000

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - function
+ * @n: variable
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int a;
+	int b = 0;
+
+	if (n > 0)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}

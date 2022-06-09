ea380421cde3d01defbcb094f498c5f732175690
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-09 17:40:01 +0000

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - function
+ *
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			_putchar('0' + b);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n')
+}

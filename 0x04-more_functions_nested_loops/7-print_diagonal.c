a02b715860af16b0b5be45cef664e5e6f7285875
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-09 20:19:22 +0000

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - function
+ * @n: n
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int a;
+	int b = 0;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < a; b++)
+				_putchar(' ');
+
+			_putchar('\');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}

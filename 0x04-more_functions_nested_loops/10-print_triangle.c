88db0757d6b525b9d26815c63452ca86010a4a35
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-10 03:38:31 +0000

    Task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle - function
+ * @size: variable
+ * Return: 0
+ */
+void print_triangle(int size)
+{
+	int a, b = 0, c;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size - a; b++)
+			{
+				_putchar(' ');
+			}
+			for (c = 0; c < a; c++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}

218e7ebeae0a372cef91be7ed684a951210e3b92
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-11 03:08:02 +0000

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,4 +8,29 @@ void print_number(int n)
 {
 	if (n < 0)
 	{
+		n *= -1;
 		_putchar('-');
+	}
+	if (n >= 0 && n < 10)
+	{
+		_putchar('0' + n);
+	}
+	if (n >= 10 && n < 100)
+	{
+		_putchar('0' + (n / 10));
+		_putchar('0' + (n % 10));
+	}
+	if (n >= 100 && n < 1000)
+	{
+		_putchar('0' + (n / 100));
+		_putchar('0' + ((n / 10) % 10));
+		_putchar('0' + (n % 10));
+	}
+	if (n >= 1000 && n < 10000)
+	{
+		_putchar('0' + (n / 1000));
+		_putchar('0' + ((n / 100) % 10));
+		_putchar('0' + ((n / 10) % 10));
+		_putchar('0' + (n % 10));
+	}
+}

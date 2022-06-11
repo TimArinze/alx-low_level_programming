1d71bb4df58556a567c56f2938461ab9f39936a5
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-11 04:06:05 +0000

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -19,7 +19,7 @@ void print_number(int n)
 	 * First remove the last digit of number and print the remaining
 	 * digits using recursion, then print the last digit
 	 */
-	if (n > 10)
+	if (n / 10)
 	{
 		print_number(n / 10);
 	}

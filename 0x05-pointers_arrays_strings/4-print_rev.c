6d90f7c664360238bbaf291587bf9bb744d636a5
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-13 18:31:59 +0000

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,14 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_rev - prints string in reverse
  * @s: string
  */
 void print_rev(char *s)
 {
-	int a;
+	int a, length;
 
-	s[a] == '\0';
-	while (a >= 0)
+	length = strlen(s);
+	a = length;
+	while (s[a])
 	{
 		_putchar(a);
 		a--;

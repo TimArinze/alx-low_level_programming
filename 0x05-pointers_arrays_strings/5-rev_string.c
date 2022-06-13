c54ec9d0fe7bee503a091c709a5a64e8ea87b694
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-13 19:30:04 +0000

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - reverse string function
+ * @s: parameter
+ */
+void rev_string(char *s)
+{
+	int a = 0;
+
+	while (*s != 0)
+	{
+		s++;
+	}
+	while (s > 0)
+	{
+		s--;
+		_putchar(*s);
+	}
+	_putchar('\n');
+}

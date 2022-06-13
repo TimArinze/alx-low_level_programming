07f30e515316c7db1703a8298c160aeead25769e
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-13 22:09:45 +0000

    Task 5

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character from first
+ * @str: String
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int a;
+
+	a = 0;
+	while (str[a])
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}

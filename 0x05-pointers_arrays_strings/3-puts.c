27114b32efa22148c38ca8849c9c8496334b8579
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-13 17:36:20 +0000

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - function that prints a string
+ * @str: parameter
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int a;
+
+	a = 0;
+	while (str[a])
+	{
+		return (str[a]);
+		a++;
+	}
+}

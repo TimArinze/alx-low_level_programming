30e60de5bc5621d935c8071c21629561f6404137
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-13 16:44:50 +0000

    Task 1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string
+ * @s: string to be counted
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int a, length;
+
+	for (a = 0; s[a] != '\0'; ++a)
+		length = a;
+	return (a);
+}

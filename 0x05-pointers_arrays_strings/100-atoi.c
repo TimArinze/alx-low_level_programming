98e7b8188b106810ce020b7476d51c838ac79b33
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-14 19:48:43 +0000

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,9 +8,24 @@
  */
 int _atoi(char *s)
 {
-	int a = 0, i;
+	int i;
+	int res = 0;
+	int sig = -1;
+	int brk = 0;
+
 
 	for (i = 0; s[i] != '\0'; ++i)
-		a = a * 10 + s[i] - '0';
-	return (a);
+	{
+		if (s[i] == '-')
+			sig = sig * -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			res = res * 10 + s[i] - '0';
+			brk = 1;
+		}
+		else if (brk == 1)
+			break;
+	}
+	res = sig * res;
+	return (res);
 }

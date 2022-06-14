56b442ef044b5b8b015f677aea891eaa6d0288b7
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-14 19:35:09 +0000

    Task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _atoi - function that converts
+ * a string to an interger
+ * @s: string parameter
+ * Return: 0
+ */
+int _atoi(char *s)
+{
+	int a = 0;
+
+	for (int i = 0; s[i] != '\0'; ++i)
+		a = a * 10 + s[i] - '0';
+	return (a);
+}

dae41798b59f08fb49b086731edb138d936b1ecc
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-14 03:12:28 +0000

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,22 +9,21 @@ void puts_half(char *str)
 	int a, len, n;
 
 	a = 0;
-	while (str[a] != 0)
+	while (str[a] != '\0')
 	{
-		len = a;
 		a++;
 	}
-	if (len % 2 != 0)
+	if (a % 2 != 0)
 	{
-		n = (len - 1) / 2;
+		n = (a - 1) / 2;
 	}
-	else if (len == 0)
+	else if (a == 0)
 	{
-		n = len;
+		n = a;
 	}
 	else
 	{
-		n = len / 2;
+		n = a / 2;
 	}
 	a = n;
 	while (str[a] != '\0')

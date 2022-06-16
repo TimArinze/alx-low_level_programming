fd043d242e93c1a7a76a2df004bb1f17bef751da
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-16 15:27:48 +0000

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * string_toupper - function that changes all
+ * lower case to upper case
+ * @s: the parameter type expected
+ * Return: char
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s -= 32;
+	}
+	return (s);
+}

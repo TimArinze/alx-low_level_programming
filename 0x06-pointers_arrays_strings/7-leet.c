b88c9cd71ae4fe48cc9e7960cf72ef297b6a73b0
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-16 18:59:12 +0000

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * leet - function that replaces certain values with numbers
+ * @s: string
+ * Return: char
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char lower[] = "aeotl";
+	char upper[] = "AEOTL";
+	char number[] = "43071";
+
+	for (i = 0; s[i] = '\0'; i++)
+	{
+		for (j = 0; (upper[j] || lower[j]) != '\0'; j++)
+		{
+			if (s[i] == (lower[j] || upper[j]))
+				s[i] = number[j];
+		}
+	}
+	return (s);
+}

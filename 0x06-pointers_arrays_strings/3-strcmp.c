d545729891c0d2ac93fb3b0f787db8a5c0777130
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-16 01:56:06 +0000

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,8 +9,8 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 == *s2++)
-		if (*s1++ == 0)
+	for (; *s1 == *s2; s1++, s2++)
+		if (*s1 == '\0')
 			return (0);
-	return (*(unsigned char *)s1 - *(unsigned char *)--s2);
+	return (*s1 - *s2);
 }

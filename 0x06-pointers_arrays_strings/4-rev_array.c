b19de94c0491c164a01c2ca385b78da7bffc01ca
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-16 14:06:09 +0000

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,10 +8,11 @@
 void reverse_array(int *a, int n)
 {
 	int i, temp;
-	for (int i = 0; i < n/2; i++)
+
+	for (i = 0; i < n/2; i++)
 	{
 		temp = a[i];
 		a[i] = a[n - 1 - i];
-		a[n - 1 - i];
+		a[n - 1 - i] = temp;
 	}
 }

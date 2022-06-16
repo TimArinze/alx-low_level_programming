54ac5cafcb176df64750d0153dbbbbb5b95e349c
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-16 15:59:16 +0000

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * cap_string - function that capitalizes all words
+ * @s: the string
+ * Return: char
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if !((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= '0' && s[i] <= '9'))
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				s[i + 1] -= 32;
+	}
+	return (s);
+}

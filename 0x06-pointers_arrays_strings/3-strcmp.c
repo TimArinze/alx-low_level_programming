3623a2500fca3ebf3b980af1df36c323eccc526d
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-16 01:21:42 +0000

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strcmp - function that compares two strings
+ * @s1: strings to be compared
+ * @s2: strings to be compared
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+
+	a = s1 - s2;
+	return (a);
+}

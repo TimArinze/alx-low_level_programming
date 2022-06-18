85729c8f068389acc6141e641e1cc63b959ff170
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-18 04:36:02 +0000

    Task 11

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * infinite_add - function that adds 2 numbers
+ * and return string
+ *
+ * @n1: number to be added
+ * @n2: number to be added
+ * @r: result after addition
+ * @size_r: size of expected result
+ * Return: char
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * print_buffer - function that prints a buffer
+ *
+ * @b: parameter
+ * @size: the buffer size
+ *
+ * Return: void
+ */

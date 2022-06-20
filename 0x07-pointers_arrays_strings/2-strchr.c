6cff25ebccf98bb319fb8d6ead6a93ba079718cb
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-20 22:22:57 +0000

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,7 +13,7 @@ char *_strchr(char *s, char c)
 	if (*s == c)
 		return (NULL);
 
-	for (i = 0; *s != '\0'; ++i, ++s)
-		if (*s == c)
+	for (i = 0; s[i] != '\0'; ++i)
+		if (s[i] == c)
 			return (s);
 }

2e9c4cc0051e0eafe13e2d5f82ae79b276bcb5f4
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-20 21:43:15 +0000

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strchr - function that locates a
+ * character in a string
+ * @s: string
+ * @c: char
+ * Return: char
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	if (s == NULL)
+		return (NULL);
+	else
+		return (s);
+}

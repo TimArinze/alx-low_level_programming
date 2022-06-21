ec823dd754e12ede99078c1326c29e455cfb047b
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-21 02:13:53 +0000

    task 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,11 +8,11 @@ void print_chessboard(char (*a)[8])
 {
 	int c, r;
 
-	for (r = 0; r < 8 && a[r] != '\0'; r++)
+	for (r = 0; r < 8; r++)
 	{
 		for (c = 0; c < 8; c++)
 		{
-			_putchar('0' + a[r][c]);
+			_putchar(a[r][c]);
 		}
 		_putchar('\n');
 	}

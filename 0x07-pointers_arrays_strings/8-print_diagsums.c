5a9526f81d6bbcbd9f0ce37a472d7205b59263e3
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-21 03:51:43 +0000

    task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,23 +9,15 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, sum1, sum2;
+	int i, sum1, sum2;
 
 	sum1 = 0;
 	sum2 = 0;
 	for (i = 0; i < size; ++i)
 	{
-		for (j = 0; j < size; ++j)
-		{
-			if (i == j)
-			{
-				sum1 = sum1 + a[i][j];
-			}
-			if (i + j == size - 1)
-			{
-				sum2 = sum2 + a[i][j];
-			}
-		}
+		sum1 += a[i];
+		sum2 += a[size - i - 1];
+		a += size;
 	}
 	printf("%d, %d\n", sum1, sum2);
 }

889016cd5994a03197500607e692a8adc57333a9
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-22 16:07:55 +0000

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,9 +8,11 @@ void _puts_recursion(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	_putchar(s[i]);
+
+	if (s[i] != '\0')
 	{
-		_putchar(s[i]);
+		_puts_recursion(*s);
 	}
 	_putchar('\n');
 }

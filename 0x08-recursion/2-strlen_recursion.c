e6ddba0358b4680cdf869b6328e381f1cc96a930
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-22 23:17:07 +0000

    Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,9 +9,12 @@ int _strlen_recursion(char *s)
 	int i;
 
 	i = 0;
-	if (*s != '0')
+	if (*s == '\0')
+		return (0);
+	if (*s != '\0')
 	{
-		i += _strlen_recursion(s + 1);
+		_strlen_recursion(s + 1);
+		i++
 	}
 	return (i);
 }

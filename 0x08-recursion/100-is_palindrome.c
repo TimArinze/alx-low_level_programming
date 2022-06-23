60afc9f848d62d855da329758b1cdb1eaea29017
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-23 18:13:44 +0000

    Task 100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "string.h"
 /**
  * is_palindrome - A function that returns 1
  * if a string is a palindrome and 0 if not
@@ -7,8 +8,24 @@
  */
 int is_palindrome(char *s)
 {
-	if (*s == '\0')
-		return (1);
-	if (*s != '\0')
+	int i, j;
+
+	i = 0;
+	j = strlen(*s) - 1;
+	return (i, j);
+}
+/**
+ * check_palindrome - function
+ * @s: string
+ * @i: interger
+ * @j: interger
+ * Return: int
+ */
+int check_palindrome(char *s, int i, int j)
+{
+	if (s[i] != s[j])
 		return (0);
+	if (s[i] == s[j])
+		return (1);
+	return (check_palindrome(s, --i, --j));
 }

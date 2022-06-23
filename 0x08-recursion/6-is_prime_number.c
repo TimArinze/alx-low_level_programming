0725777d1473f61d5c3fea86fa79a6176731c6e8
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-23 02:50:19 +0000

    Task 4

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * is_prime_number - function that returns 1
+ * @n: number to be checked
+ * Return: int
+ */
+int is_prime_number(int n)
+{
+	return (prime(n, 2));
+}
+/**
+ * prime - function of prime number
+ * @n: the number
+ * @div: the divisor
+ * Return: int
+ */
+int prime(int n, int div)
+{
+	if (div * div > n)
+		return (1);
+	else
+		return (0);
+	return (prime(n, div + 1));
+}

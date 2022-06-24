1ebd63d2da0e624539c7a6efcc8a5b10b46e2f5b
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-24 11:30:30 +0000

    Task 0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry function
+ * @argv: string array
+ * @argc: counter
+ * Return: 0
+ */
+int main(int argc, char *argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}

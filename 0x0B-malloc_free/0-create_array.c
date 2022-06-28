2184c130b91c6851eb4ed46f2f60f27413429cec
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-28 22:17:53 +0000

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 /**
  * create_array - function that creates an array of chars and initializes
  * it with a specific char
@@ -8,12 +9,10 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char p;
 
 	size = sizeof(char) * 1;
-	p = &c;
 	if (size == 0)
 		return (NULL);
 	else
-		return (*p);
+		return (*c);
 }

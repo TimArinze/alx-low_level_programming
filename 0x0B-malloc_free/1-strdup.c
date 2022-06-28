5c34a4f04154c2f595c3d53a75f82a9049e552e2
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-28 23:36:50 +0000

    Task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,13 +8,18 @@
  */
 char *_strdup(char *str)
 {
-	int len;
+	int new_len, size;
 	char *copy;
 
-	len = strlen(str) + 1;
-	copy = malloc(len);
-
-	if (copy == NULL)
+	if (str == NULL)
 		return (NULL);
+	for (size = 0; str[size] != 0; size++);
+
+	copy = malloc(size * sizeof(*str) + 1);
+	if (copy == 0)
+		return (NULL);
+	else
+		for (new_len = 0; new_len < size; new_len++)
+			copy[new_len] = str[size];
 	return (copy);
 }

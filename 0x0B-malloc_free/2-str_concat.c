d4e1d373dd715ec7b2d96ccac5c2cac31760a844
Arinze Nwutobo <tim.arinze@outlook.com>
2022-06-29 00:59:14 +0000

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,7 +17,7 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = '\0';
 	if (s1 == NULL && s2 == NULL)
-		s1 + s2 = '\0';
+		return (NULL);
 	for (size2 = 0; s2[size2] != '\0'; size2++)
 		;
 	for (size1 = 0; s1[size1] != '\0'; size1++)

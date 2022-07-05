35751d78b19d919fe544337c792ad2feb6276d99
Arinze Nwutobo <tim.arinze@outlook.com>
2022-07-05 10:50:02 +0000

    Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+/**
+ * init_dog - the function
+ * struct dog - the struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * @d: struct
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog *d;
+}

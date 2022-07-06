b564fb5babb57d2e815eb850c11de83cee1d1371
Arinze Nwutobo <tim.arinze@outlook.com>
2022-07-06 00:49:52 +0000

    Task 4

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * free_dog - the function
+ * @d: the dog
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d->age);
+	free(d);
+}

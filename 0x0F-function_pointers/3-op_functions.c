a54babcaf7d12b6434a98fab0361b70afd21feb9
Arinze Nwutobo <tim.arinze@outlook.com>
2022-07-07 00:47:47 +0000

    Task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * op_add - function that adds
+ * @a: integers for addition
+ * @b: integers for addition
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - function that subtract
+ * @a: integer
+ * @b: integre
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function that multiple
+ * @a: integer
+ * @b: integer
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - function
+ * @a: integer
+ * @b: integer
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - function
+ * @a: integer
+ * @b: integer
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

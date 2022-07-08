b93c521f9f7017eb4f9249b5bf9f426926243e5d
Arinze Nwutobo <tim.arinze@outlook.com>
2022-07-08 01:13:51 +0000

    Task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,75 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_int - function
+ * @ptr
+ */
+void print_int(va_list ptr)
+{
+	printf("%i", va_arg(ptr, int));
+}
+/**
+ * print_float - function
+ * @ptr
+ */
+void print_float(va_list ptr)
+{
+	printf("%f", va_arg(ptr, double));
+}
+/**
+ * print_char - function
+ * @ptr
+ */
+void print_char(va_list ptr)
+{
+	printf("%c", va_arg(ptr, int));
+}
+/**
+ * print_str - function
+ * @ptr
+ */
+void print_str(va_list ptr)
+{
+	char *c = va_arg(ptr, char *);
+	if (c == NULL)
+		printf("(nil)");
+	else
+		printf("%s", c);
+}
+/**
+ * print_all - function
+ * @format:
+ */
+void print_all(const char * const format, ...)
+{
+	int i, j;
+	va_list ptr;
+	char *separator = "";
+
+	printTypeStruct printType[] = {
+		{"i", print_int},
+		{"f", print_float},
+		{"c", print_char},
+		{"s", print_str},
+		{NULL, NULL}
+	};
+
+	va_start(ptr, format);
+
+	for (i = 0; format && format[i]; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			if (*printType[j].type == format[i])
+			{
+				printf("%s", separator);
+				separator = ", ";
+				break;
+			}
+		}
+	}
+	printf("\n");
+	va_end(ptr);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,25 @@
 #ifndef VARIADIC_H
 #define VARIADIC_H
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
+
+void print_int(va_list ptr);
+void print_float(va_list ptr);
+void print_char(va_list ptr);
+void print_str(va_list ptr);
+/**
+ * struct printTypeStruct - struct
+ * @type: type
+ * @printer: function to print
+ */
+typedef struct printTypeStruct
+{
+	char *type;
+	void (*printer)(va_list);
+} printTypeStruct;
 
 #endif

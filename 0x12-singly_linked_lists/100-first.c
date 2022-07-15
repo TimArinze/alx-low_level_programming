aae2587823a01ec32986f6b093386593bc2ea5ce
Arinze Nwutobo <tim.arinze@outlook.com>
2022-07-15 02:30:35 +0000

    task 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,6 +5,6 @@ void premain(void) __attribute__((constructor));
  */
 void premain(void)
 {
-	printf("You're beat! and yet, you must")
+	printf("You're beat! and yet, you must ");
 	printf("allow,\nI bore my house upon my back!\n");
 }

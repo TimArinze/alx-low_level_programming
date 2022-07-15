04e6aa6a0834b5aebedd636e8dcf95c3fae53426
Arinze Nwutobo <tim.arinze@outlook.com>
2022-07-15 02:13:47 +0000

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "list.h"
+/**
+ * first_print - a function that prints a statement below
+ */
+void first_print(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my
+			back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -21,5 +21,6 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 int _putchar(char c);
+void first_print(void);
 
 #endif

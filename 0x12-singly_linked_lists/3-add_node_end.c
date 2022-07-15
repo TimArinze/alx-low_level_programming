e5e830ac57849c200736acb687e905e42a3f2d1e
Arinze Nwutobo <tim.arinze@outlook.com>
2022-07-15 01:57:20 +0000

    task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -34,7 +34,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (temp);
 	}
 	temp1 = *head;
-       while (temp1->next)
+	while (temp1->next)
 		temp1 = temp1->next;
 	temp1->next = temp;
 	return (temp);

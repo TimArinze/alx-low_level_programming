3783fefd12182404c28258647e5881e1462a8247
Arinze Nwutobo <tim.arinze@outlook.com>
2022-07-19 15:06:59 +0000

    task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,7 +15,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	temp = malloc(sizeof(listint_t));
 	if (temp == NULL)
+	{
+		free(temp);
 		return (NULL);
+	}
 
 	temp->n = n;
 	temp->next = NULL;

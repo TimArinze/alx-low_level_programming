f08bde0a092e4731e31e44a3b7fc0dff45cf00ca
Arinze Nwutobo <tim.arinze@outlook.com>
2022-07-21 05:09:29 +0000

    Task 9

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,7 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		if ((*head)->next)
-			*head = (*head)->next;
+			(*head) = (*head)->next;
 		else
 			(*head) = NULL;
 		free(current);

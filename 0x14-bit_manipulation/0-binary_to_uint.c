f928b47fe6ae994db58588048c0f9be1ea099509
Arinze Nwutobo <tim.arinze@outlook.com>
2022-07-21 22:30:53 +0000

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * binary_to_uint - Function that converts a binary
+ * to an unsigned int
+ * @b: string to be convert
+ *
+ * Return: the converted number or 0 if in the string
+ * is not 0 or 1
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int binary, reminder, weight = 1, decimal = 0;
+
+	if (b == NULL)
+		return (0);
+
+	binary = atoi(b);
+
+	while (binary != 0)
+	{
+		reminder = binary % 10;
+		if (reminder < 2)
+		{
+			decimal  = decimal + (weight * reminder);
+			binary = binary / 10;
+			weight = weight * 2;
+		}
+		else
+			return (0);
+	}
+	return (decimal);
+}

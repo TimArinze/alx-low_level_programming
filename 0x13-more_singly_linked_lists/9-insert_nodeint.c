bfd4783867cb40318a0393d1b791d8a7fc0c80ef
Arinze Nwutobo <tim.arinze@outlook.com>
2022-07-21 03:34:25 +0000

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,7 +37,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (idx != 1)
 	{
 		ptr = ptr->next;
-		if (ptr = NULL)
+		if (ptr == NULL)
 		{
 			free(temp);
 			return (temp);

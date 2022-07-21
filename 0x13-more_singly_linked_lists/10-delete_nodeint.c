01c89e4201f0f63a8daf086dcb0b5b6c46138309
Arinze Nwutobo <tim.arinze@outlook.com>
2022-07-21 03:48:24 +0000

    Task 9

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - As the name
+ * @head: head
+ * @index: index of node to be deleted
+ * Return: 1 for successful and -1 for failure
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -40,7 +40,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (ptr == NULL)
 		{
 			free(temp);
-			return (temp);
+			return (NULL);
 		}
 		--idx;
 	}

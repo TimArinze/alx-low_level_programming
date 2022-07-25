17fce9d52a82f0e8e301962ea12d9e9f1969cc6e
Arinze Nwutobo <tim.arinze@outlook.com>
2022-07-25 20:01:26 +0000

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+/**
+ * create_file - function that creates a file
+ * @filename: for the file name
+ * @text_content: is NULL
+ * Return: 1 on success and -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i, filestatus;
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+		filestatus = write(fd, text_content, i);
+		if (filestatus == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}

98945921ccf8ed127bacfed8c3e9b2b75bf6509a
Arinze Nwutobo <tim.arinze@outlook.com>
2022-07-25 23:11:24 +0000

    Task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#define SE STDERR_FILENO
+/**
+ * main - Entry function
+ *
+ * @argc: the number
+ * @argv: array of string
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int input_fd, output_fd, istatus, ostatus;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(SE, "Usage: cp file_from file_to\n");
+		/* SE is for standard error */
+		exit(97);
+	}
+	input_fd = open(argv[1], O_RDONLY);
+	if (input_fd == -1)
+	{
+		dprintf(SE, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	output_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 00664);
+	if (out_fd == -1)
+	{
+		dprintf(SE, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	do {
+		istatus = read(input_fd, buffer, 1024);
+		if (istatus == -1)
+		{
+			dprintf(SE, "Error:Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		ostatus = write(output_fd, buffer, 1024);
+		if (ostatus == -1)
+		{
+			dprintf(SE, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	while (istatus > 0);
+
+
+	istatus = close(input_fd);
+	if (istatus == -1)
+	{
+		dprintf(SE, "Error: Can't close fd %d\n", input_fd);
+		exit(100);
+	}
+	ostatus = close(output_fd);
+	if (ostatus == -1)
+	{
+		dprintf(SE, "Error: Can't close fd %d\n", output_fd);
+		exit(100);
+	}
+
+	return (0);
+}

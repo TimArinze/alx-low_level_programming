5e7609ed6554fc7809001d6e4a5e3bcc1df288a6
Arinze Nwutobo <tim.arinze@outlook.com>
2022-07-26 00:12:48 +0000

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -26,7 +26,7 @@ void close_file(int fd)
 int main(int argc, char *argv[])
 {
 	int input_fd, output_fd, istatus, ostatus;
-	char buffer[1024];
+	char *buffer;
 	mode_t mode;
 
 	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
@@ -35,6 +35,7 @@ int main(int argc, char *argv[])
 		dprintf(SE, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	buffer = malloc(sizeof(char) * 1024);
 	input_fd = open(argv[1], O_RDONLY);
 	if (input_fd == -1)
 	{
@@ -51,12 +52,14 @@ int main(int argc, char *argv[])
 		istatus = read(input_fd, buffer, 1024);
 		if (istatus == -1)
 		{
+			free(buffer);
 			dprintf(SE, "Error:Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
 		ostatus = write(output_fd, buffer, 1024);
 		if (ostatus == -1)
 		{
+			free(buffer);
 			dprintf(SE, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}

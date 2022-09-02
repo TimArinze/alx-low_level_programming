eaaa64276998fe0169147c32234cdb6570b57260
Arinze Nwutobo <tim.arinze@outlook.com>
2022-09-02 15:30:02 +0000

    task

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - function that returns
+ * the nth node of a dlistint_t linked list
+ * @head: head
+ * @index: the index
+ * Return: NULL if node doesnt exist
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	for (; head->next != NULL && i != index; i++)
+		head = head->next;
+	if (index > i)
+		return (NULL);
+	else
+		return (head);
+}

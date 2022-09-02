de010085d3376ecd4b5ea201dda644b6c5616241
Arinze Nwutobo <68851511+TimArinze@users.noreply.github.com>
2022-09-02 04:05:26 +0100

    del

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++
@@ -1,8 +0,0 @@
-#include "lists.h"
-/**
- * add_dnodeint_end - function that adds a new node at the end of doubly list
- * @head: head
- * @n: the data
- * Return: address
- */
-dlistint_t *add_dnodeint_end(dlistint_t **head, con

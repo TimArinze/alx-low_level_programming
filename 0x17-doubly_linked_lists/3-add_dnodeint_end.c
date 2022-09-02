130ffa684076fae5789de846d60b07e5d94b89eb
Arinze Nwutobo <tim.arinze@outlook.com>
2022-09-01 20:23:21 -0700

    task

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -26,6 +26,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		temp = temp->next;
 	temp->next = new;
 	new->prev = temp;
-	*head = new;
-	return (*head);
+	temp = new;
+	return (temp);
 }

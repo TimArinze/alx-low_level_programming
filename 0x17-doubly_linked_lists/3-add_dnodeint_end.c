2a4b9f42c90afe0767984c0e05d5dd4f04b4bea7
Arinze Nwutobo <tim.arinze@outlook.com>
2022-09-01 20:51:21 -0700

    task

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,22 +10,21 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new;
 	dlistint_t *temp;
 
-	temp = *head;
-	while (temp && temp->next != NULL)
-		temp = temp->next;
-
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-	{
-		free(new);
 		return (NULL);
-	}
 	new->n = n;
 	new->next = NULL;
-	new->prev = temp;
-	if (temp)
-		temp->next = new;
-	else
+	if (*head == NULL)
+	{
 		*head = new;
-	return (new);
+		new->prev = NULL;
+		return (new);
+	}
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp->next = new;
+	new->prev = temp;
+	return (temp);
 }

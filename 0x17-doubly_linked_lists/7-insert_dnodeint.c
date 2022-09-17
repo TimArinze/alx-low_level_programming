60d37c3b697568f3651e818f2fe2fadf35cd8cd3
Arinze Nwutobo <tim.arinze@outlook.com>
2022-09-17 00:10:49 +0000

    task

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,6 +12,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *curr, *new;
 
 
+	if (*h == NULL)
+		return (NULL);
 	curr = *h;
 	while (idx != 0)
 	{
@@ -31,5 +33,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->prev = curr->prev;
 	if (curr->prev != NULL)
 		curr->prev->next = new;
-	return (curr);
+	return (new);
 }

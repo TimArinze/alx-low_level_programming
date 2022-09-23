412229dadaaf58562399aca3e9dae51321c89d0f
Arinze Nwutobo <tim.arinze@outlook.com>
2022-09-23 15:36:14 +0000

    task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,13 +9,19 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new;
 
-	if (!size)
+	if (size == 0)
 		return (NULL);
 
 	new = malloc(sizeof(hash_table_t));
 	if (new == NULL)
 		return (NULL);
 
-	new->size = new;
+	new->size = size;
+	new->array = calloc(size, sizeof(hash_node_t *));
+	if (new->array == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 	return (new);
 }

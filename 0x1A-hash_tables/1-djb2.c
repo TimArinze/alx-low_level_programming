5d3ada205f936905028d8129ab2c47be190e12ac
Arinze Nwutobo <tim.arinze@outlook.com>
2022-09-23 19:52:04 +0000

    task

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,10 +1,14 @@
+#include "hash_tables.h"
+/**
+ * hash_djb2 - A function implementing the djb2 algorithm
+ * @str: string
+ * Return: hash
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
 	int c;
 
-	if (*str == NULL)
-		return (NULL);
 	hash = 5381;
 	while ((c = *str++))
 	{

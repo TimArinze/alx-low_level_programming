53efb757efd1e7f4a36e30482d063ae1a0b79177
Arinze Timothy Nwutobo <timothyarinze@gmail.com>
2022-09-29 02:20:21 +0000

    task

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,60 +9,30 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *node;
-	hash_node_t *new_node;
-
-	if (ht == NULL || *key == '\n' || *value == '\n')
-		return (0);
-	index = key_index((const unsigned char *)key, ht->size);
-	node = ht->array[index];
-
-	if (node == NULL)
-	{
-		new_node = create_new_node(key, value);
-		if (new_node == NULL)
-			return (0);
-		ht->array[index] = new_node;
-		return (1);
-	}
-	/* if key exists, replace value */
-	while (node != NULL)
-	{
-		if (strcmp(key, node->key) == 0)
-		{
-			free(node->value);
-			node->value = strdup(value);
-			return (1);
-		}
-		node = node->next;
-	}
-	/* if key doesn't exist, create new node */
-	new_node = create_new_node(key, value);
-	if (new_node == NULL)
-		return (0);
-
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
-	return (1);
-}
-
-/**
- * create_new_node - create a new node
- * @key: is the key. key can not be an empty string
- * @value: value of the key
- * value must not be duplicated. value can be an empty string
- * Return: pointer to the new node
- */
-hash_node_t *create_new_node(const char *key, const char *value)
-{
 	hash_node_t *new;
+
+	if (ht == NULL || key == NULL)
+		return (0);
 
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
-		return (NULL);
+		return (0);
+
 	new->key = strdup(key);
 	new->value = strdup(value);
 	new->next = NULL;
 
-	return (new);
+	index = key_index((const unsigned char *)key, ht->size);
+
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = new;
+		return (1);
+	}
+	if (ht->array[index] != NULL)
+	{
+		new->next = ht->array[index];
+		ht->array[index] = new;
+	}
+	return (1);
 }

79bbb252c8d0674cb2fec31f1872e6f9cafa440f
Arinze Timothy Nwutobo <timothyarinze@gmail.com>
2022-10-10 01:49:04 +0000

    task

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -31,7 +31,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	if (ht->array[index] != NULL)
 	{
-		if (ht->array[index]-> != key)
+		if (ht->array[index]->key != key)
 		{
 			new->next = ht->array[index];
 			ht->array[index] = new;

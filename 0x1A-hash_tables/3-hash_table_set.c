99a65dc947345695536e4a23081c5900962ee5de
Arinze Timothy Nwutobo <timothyarinze@gmail.com>
2022-10-10 01:44:41 +0000

    task

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -31,7 +31,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	if (ht->array[index] != NULL)
 	{
-		if (strcmp(key, ht->array[index]->key) == 0)
+		if (ht->array[index]->key == key)
 		{
 			free(ht->array[index]);
 			ht->array[index] = new;
--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,3 +1,7 @@
 #include "hash_tables.h"
 /**
- *
+ * hash_table_get - A function that retrieves a value
+ * associated with a key
+ * @ht: Hash Table
+ * @key: Key
+ * Return: Value or NULL if the key is not found

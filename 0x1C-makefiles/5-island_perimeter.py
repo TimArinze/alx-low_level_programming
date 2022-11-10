432cc6268a625c0663cff9626604dba5f1c6c8fc
Arinze Nwutobo <tim.arinze@outlook.com>
2022-11-10 11:45:31 +0100

    task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+Create a function def island_perimeter(grid):
+that returns the perimeter of the island described in grid
+"""
+
 
 def island_perimeter(grid):
+    """ island_perimeter """
 
     perimeter = 0
     for i in range(len(grid)):

819b37076bdccf532f420356798977065e7dd728
Arinze Nwutobo <tim.arinze@outlook.com>
2023-01-02 14:26:55 +0100

    task

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,20 @@
+#include "search_algos.h"
+/**
+ * linear_search - function that searches for a value in an array of integers
+ * @array: The array to be searched
+ * @size: The size of the array
+ * @value: value to search
+ * Return: if value not present or NULL return -1 else the first index where
+ * the value is located
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}

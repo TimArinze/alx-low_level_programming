0cb944989939a9884bc0a93a7dc01ba40aeb2a01
Arinze Nwutobo <tim.arinze@outlook.com>
2023-01-03 20:11:02 +0100

    task

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,14 +9,11 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int high, i;
+	size_t i;
 
-	high = size;
-	if (array == NULL)
-		return (-1);
-	for (i = 0; i < high; i++)
+	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}

f091d5b021e12ec79e6b82516a6392fc5d332bc0
Arinze Nwutobo <tim.arinze@outlook.com>
2023-01-03 20:45:04 +0100

    task

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -8,19 +8,59 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int mid, low, high;
+	int mid, low, high, j;
 
-	low = array[0];
-	high = array[size - 1];
+	low = 0;
+	high = size - 1;
+	print(array, size);
 	while (low != high)
 	{
 		mid = (low + high) / 2;
 		if (value == array[mid])
-			return (mid);
-		else if (value > mid)
+			return (array[mid]);
+		else if (value > array[mid])
+		{
 			low = mid + 1;
+			printf("Searching in array:");
+			for (j = low; j <= high; j++)
+			{
+				if (j == high)
+					printf(" %d\n", array[j]);
+				else
+					printf(" %d,", array[j]);
+			}
+		}
 		else
+		{
 			high = mid - 1;
+			printf("Searching in array:");
+			for (j = low; j <= high; j++)
+			{
+				if (j == high)
+					printf(" %d\n", array[j]);
+				else
+					printf(" %d,", array[j]);
+			}
+		}
 	}
 	return (-1);
+}
+
+/**
+ * print - prints the array
+ * @array: the array
+ * @size: the size
+ */
+void print(int *array, size_t size)
+{
+	size_t i;
+
+	printf("Searching in array:");
+	for (i = 0; i < size; i++)
+	{
+		if (i == size - 1)
+			printf(" %d\n", array[i]);
+		else
+			printf(" %d,", array[i]);
+	}
 }

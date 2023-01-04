ca9803a1c5eeef4888515325e00af2eb24e57bbf
Arinze Nwutobo <tim.arinze@outlook.com>
2023-01-04 20:16:44 +0100

    task

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -8,59 +8,25 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int mid, low = 0, high = size - 1, j;
+	int mid, low = 0, high = size - 1, i;
 
-	if (array == NULL)
+	if (!array)
 		return (-1);
-	print(array, size);
-	while (low != high)
+	while (low <= high)
 	{
+		printf("Searching in array:");
+		for (i = low; i < high; i++)
+		{
+			printf(" %d,", array[i]);
+		}
+		printf(" %d\n", array[high]);
 		mid = (low + high) / 2;
 		if (value == array[mid])
 			return (array[mid]);
 		else if (value > array[mid])
-		{
 			low = mid + 1;
-			printf("Searching in array:");
-			for (j = low; j <= high; j++)
-			{
-				if (j == high)
-					printf(" %d\n", array[j]);
-				else
-					printf(" %d,", array[j]);
-			}
-		}
-		else
-		{
+		else if (value < array[mid])
 			high = mid - 1;
-			printf("Searching in array:");
-			for (j = low; j <= high; j++)
-			{
-				if (j == high)
-					printf(" %d\n", array[j]);
-				else
-					printf(" %d,", array[j]);
-			}
-		}
 	}
 	return (-1);
-}
-
-/**
- * print - prints the array
- * @array: the array
- * @size: the size
- */
-void print(int *array, size_t size)
-{
-	size_t i;
-
-	printf("Searching in array:");
-	for (i = 0; i < size; i++)
-	{
-		if (i == size - 1)
-			printf(" %d\n", array[i]);
-		else
-			printf(" %d,", array[i]);
-	}
 }

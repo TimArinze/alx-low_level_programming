7345509d77bed2b53dacf57d1caf8cc9cfb843b7
Arinze Nwutobo <tim.arinze@outlook.com>
2024-12-27 00:28:04 +0100

    Fixing

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,11 +11,7 @@ def island_perimeter(grid):
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if grid[0][j] == 1:
-                return 0
-            elif grid[i][0] == 1:
-                return 0
-            elif grid[i][j] == 1:
+            if grid[i][j] == 1:
                 if grid[i][j-1] == 0:
                     perimeter = perimeter + 1
                 else:

5dabc0b09c3231107190971900d6b35e5721f45f
Arinze Nwutobo <tim.arinze@outlook.com>
2024-12-27 16:27:53 +0100

    fixing

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,25 +12,29 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if grid[i][j-1] == 0:
+                if j == 0:
+                    perimeter = perimeter + 1
+                elif grid[i][j-1] == 0:
                     perimeter = perimeter + 1
                 else:
                     perimeter = perimeter
-                if grid[i][j+1] == 0:
+                if j == len(grid[i]) - 1:
                     perimeter = perimeter + 1
-                else:
-                    perimeter = perimeter
-                if grid[i-1][j] == 0:
-                    perimeter = perimeter + 1
-                else:
-                    perimeter = perimeter
-                if i + 1 < len(grid):
-                    if grid[i+1][j] == 0:
+                elif grid[i][j+1] == 0:
                         perimeter = perimeter + 1
-                    else:
-                        perimeter = perimeter
-                if i + 1 == len(grid):
+                else:
+                    perimeter = perimeter
+                if i == 0:
+                    perimeter = perimeter + 1
+                elif grid[i-1][j] == 0:
                     perimeter = perimeter + 1
                 else:
                     perimeter = perimeter
+                if i == len(grid) - 1:
+                    perimeter = perimeter + 1
+                elif grid[i+1][j] == 0:
+                    perimeter = perimeter + 1
+                else:
+                    perimeter = perimeter
+
     return perimeter

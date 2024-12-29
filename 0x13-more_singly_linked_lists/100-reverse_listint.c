21cd46c7755f5e1ae0b5ba512fcfacf6b80760f7
Arinze Nwutobo <tim.arinze@outlook.com>
2024-12-29 01:42:03 +0100

    fixing

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,7 +1,7 @@
 #include <stddef.h>
 #include "lists.h"
 /**
- * function - reverse_listint a function that reverses a linked list
+ * reverse_listint - a function that reverses a linked list
  * @head: The linked list that is to be reversed
  * Return: The reversed linked list
  */
@@ -20,13 +20,12 @@ listint_t *reverse_listint(listint_t **head)
 	{
 		/* To save the address of the next node */
 		temp = start->next;
-		/* To reverse where the start-next node is pointing to prev */	
+		/* To reverse where the start-next node is pointing to prev */
 		start->next = prev;
 		/* To move the previous node to the current node */
 		prev = start;
 		/* To move start to the next node saved using temp */
 		start = temp;
-
 	}
 	*head = prev;
 	return (*head);
